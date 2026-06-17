d4630badc0f602dc6f7e8f00bb0bc22b43fd7923
Allen M. Foster <amfoster@tiger3.princeton.edu>
2026-06-17 12:02:50 -0400

    some fixes to precommit and ruff

--- file_io.py
+++ file_io.py
@@ -1,21 +1,19 @@
 # imports
-import os
 import glob
+import os
 import re
+
 import h5py
 import numpy as np
-from astropy.coordinates.angles import Angle
 from astropy import units as u
 from astropy.coordinates import SkyCoord
+from astropy.coordinates.angles import Angle
 from numpy import mod as npmod
 from pixell import enmap
 
+
 # string + naming functions
-def radec_to_str_name(ra: float, 
-                      dec: float, 
-                      source_class="pointsource", 
-                      observatory="SO"
-                      ):
+def radec_to_str_name(ra: float, dec: float, source_class="pointsource", observatory="SO"):
     """
     ## stolen from spt3g_software -AF
 
@@ -40,7 +38,7 @@ def radec_to_str_name(ra: float,
         A unique identifier for the source with coordinates truncated to the
         appropriate number of significant figures for the source class.
     """
-    
+
     source_class = str(source_class).lower()
     if source_class in ["sv", "t", "tr", "transient", "v", "var", "variable"]:
         source_class = "SV"
@@ -80,12 +78,13 @@ def radec_to_str_name(ra: float,
     name = "{}-{} J{}{}".format(observatory, source_class, rastr, decstr)
     return name
 
+
 def decode_filename_to_act(source_name, band):
     """Reverses the IAU string format to standard RA/Dec degrees."""
     source_str = str(source_name)
-    if ' J' in source_str:
-        coord_str = source_str.split(' J')[1]
-        match = re.match(r'(\d{6})([+-]\d{4}\.?\d*)', coord_str)
+    if " J" in source_str:
+        coord_str = source_str.split(" J")[1]
+        match = re.match(r"(\d{6})([+-]\d{4}\.?\d*)", coord_str)
         if match:
             ra_raw, dec_raw = match.groups()
             ra_formatted = f"{ra_raw[0:2]}h{ra_raw[2:4]}m{ra_raw[4:6]}s"
@@ -94,9 +93,10 @@ def decode_filename_to_act(source_name, band):
                 c = SkyCoord(f"{ra_formatted} {dec_formatted}")
                 return f"ACT_RA{c.ra.deg:.3f}_Dec{c.dec.deg:.3f}_{band}.png"
             except ValueError:
-                pass 
+                pass
     safe_source = source_str.replace(" ", "_").replace("SO-S_", "ACT_")
     return f"{safe_source}_{band}.png"
+
 
 # hdf5 thumbnail tools
 def load_custom_hdf5(fname):
@@ -104,58 +104,62 @@ def load_custom_hdf5(fname):
     thumbnails = {}
     for key in context:
         thumb_dict = {}
-        maptype=enmap.fix_python3(context[key]['maptype'][()])
-        Id = enmap.fix_python3(context[key]['Id'][()]).split(f'_{maptype}')[0]
+        maptype = enmap.fix_python3(context[key]["maptype"][()])
+        Id = enmap.fix_python3(context[key]["Id"][()]).split(f"_{maptype}")[0]
         thumb_maps = enmap.read_hdf(context[key])
-        
+
         for k in context[key].keys():
-            if k == 'wcs' or k =='data':
+            if k == "wcs" or k == "data":
                 continue
             thumb_dict[k] = enmap.fix_python3(context[key][k][()])
-            
+
         if Id not in thumbnails:
             thumbnails[Id] = thumb_dict
         thumbnails[Id][maptype] = thumb_maps
-        
+
     return thumbnails
+
 
 def organize_thumbnails_by_source(thumbnails):
     org_thumbs = {}
     for thumb_id in thumbnails:
-        source = thumb_id.split('_')[0]
+        source = thumb_id.split("_")[0]
         if source not in org_thumbs:
             org_thumbs[source] = []
         org_thumbs[source].append(thumbnails[thumb_id])
 
     return org_thumbs
 
+
 def get_thumbnail_times(thumbnails):
     times = []
     for i in range(len(thumbnails)):
-        times.append(float(thumbnails[i]['t']))
+        times.append(float(thumbnails[i]["t"]))
     return times
+
 
 def read_thumbnails(data_dir):
     # find and sort files in one line
-    files = sorted(glob.glob(os.path.join(data_dir, 'collated_thumbnail_*.hdf5')))
+    files = sorted(glob.glob(os.path.join(data_dir, "collated_thumbnail_*.hdf5")))
     print(f"Found {len(files)} files. Loading...")
     all_data = []
     for f in files:
         thumbs = load_custom_hdf5(f)
         all_data.append(organize_thumbnails_by_source(thumbs))
     return all_data
-    
+
+
 # lightcurve tools
 def read_lightcurves(filepath):
-    data = np.loadtxt(filepath, delimiter=',', unpack=True, dtype=str)
+    data = np.loadtxt(filepath, delimiter=",", unpack=True, dtype=str)
     lc_data = {
-        'time': np.asarray(data[0], dtype=float),
-        'source_names': np.asarray(data[1], dtype=str),
-        'ra': np.asarray(data[2], dtype=float),
-        'dec': np.asarray(data[3], dtype=float),
-        'arrs': data[4],
-        'bands': data[5],
-        'flux': np.asarray(data[7], dtype=float),
-        'dflux': np.asarray(data[8], dtype=float)
+        "time": np.asarray(data[0], dtype=float),
+        "source_names": np.asarray(data[1], dtype=str),
+        "ra": np.asarray(data[2], dtype=float),
+        "dec": np.asarray(data[3], dtype=float),
+        "arrs": data[4],
+        "bands": data[5],
+        "flux": np.asarray(data[7], dtype=float),
+        "dflux": np.asarray(data[8], dtype=float),
     }
     return lc_data
--- script.py
+++ script.py
@@ -1,42 +1,37 @@
 # imports
 import argparse
-import os
+
 import numpy as np
+
 import file_io
-import processing
 import plotting
+import processing
 
 # pipeline parameters
-parser = argparse.ArgumentParser(description='ACT lightcurve viewer pipeline')
-parser.add_argument('-t',
-                    '--thumbnail-dir', 
-                    default='./',
-                    help='Directory containing thumbnail files'
-                    )
-parser.add_argument('-l',
-                    '--lightcurve-file', 
-                    default='collated_lightcurve.txt',
-                    help='Path to collated lightcurve text file'
-                    )
-parser.add_argument('-c',
-                    '--coadd-days', 
-                    type=int, 
-                    default=3,
-                    help='Number of days to coadd.'
-                    )
-parser.add_argument('-b',
-                    '--bands', 
-                    nargs='+', 
-                    default=['f090', 'f150', 'f220'],
-                    help='ACT frequency bands to process'
-                    )
+parser = argparse.ArgumentParser(description="ACT lightcurve viewer pipeline")
+parser.add_argument(
+    "-t", "--thumbnail-dir", default="./", help="Directory containing thumbnail files"
+)
+parser.add_argument(
+    "-l",
+    "--lightcurve-file",
+    default="collated_lightcurve.txt",
+    help="Path to collated lightcurve text file",
+)
+parser.add_argument("-c", "--coadd-days", type=int, default=3, help="Number of days to coadd.")
+parser.add_argument(
+    "-b",
+    "--bands",
+    nargs="+",
+    default=["f090", "f150", "f220"],
+    help="ACT frequency bands to process",
+)
 args = parser.parse_args()
 
 thumbnail_dir = args.thumbnail_dir
 lightcurve_file = args.lightcurve_file
 coadd_days = args.coadd_days
 act_bands = args.bands
-
 
 
 # phase 1: thumbnail pipeline
@@ -54,8 +49,8 @@ for file_data in all_thumbnails:
 # 3. setup variables for plotting
 sources = []
 for i in range(len(coadded_thumbnails)):
-    sources.append(coadded_thumbnails[i]['source_id'])
-sources = np.unique(sources)    
+    sources.append(coadded_thumbnails[i]["source_id"])
+sources = np.unique(sources)
 
 mean_observation_times = {}
 for source in sources:
@@ -63,9 +58,14 @@ for source in sources:
     for band in act_bands:
         mean_observation_times[source][band] = []
         for i in range(len(coadded_thumbnails)):
-            if coadded_thumbnails[i]['source_id']!=source or coadded_thumbnails[i]['freq']!=band :
+            if (
+                coadded_thumbnails[i]["source_id"] != source
+                or coadded_thumbnails[i]["freq"] != band
+            ):
                 continue
-            mean_observation_times[source][band].append(int(np.mean(coadded_thumbnails[i]['coadded_observation_times'])))
+            mean_observation_times[source][band].append(
+                int(np.mean(coadded_thumbnails[i]["coadded_observation_times"]))
+            )
 
 # 4. generate plots
 print(f"Generating thumbnail plots for {len(sources)} sources...")
@@ -80,11 +80,11 @@ print("\n=== STARTING LIGHTCURVE PROCESSING ===")
 print(f"Loading lightcurve data from: {lightcurve_file}")
 lc_data = file_io.read_lightcurves(lightcurve_file)
 
-time = lc_data['time']
-source_names = lc_data['source_names']
-bands = lc_data['bands']
-flux = lc_data['flux']
-dflux = lc_data['dflux']
+time = lc_data["time"]
+source_names = lc_data["source_names"]
+bands = lc_data["bands"]
+flux = lc_data["flux"]
+dflux = lc_data["dflux"]
 
 # 2. process math (noise cuts)
 print("Calculating dynamic noise cuts...")
